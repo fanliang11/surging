0aa3d090c429edc2d3a84fd3352251ca159eac77
fanliang11 <137629448@qq.com>
2017-11-24 17:23:08 +0800

    Add Document

--- src/Surging.Core/Surging.Core.CPlatform/Support/Implementation/BreakeRemoteInvokeService.cs
+++ src/Surging.Core/Surging.Core.CPlatform/Support/Implementation/BreakeRemoteInvokeService.cs
@@ -27,13 +27,13 @@ namespace Surging.Core.CPlatform.Support.Implementation
         public async Task<RemoteInvokeResultMessage> InvokeAsync(IDictionary<string, object> parameters, string serviceId, string serviceKey, bool decodeJOject)
         {
             var serviceInvokeInfos = _serviceInvokeListenInfo.GetOrAdd(serviceId, new ServiceInvokeListenInfo());
-            var command =await _commandProvider.GetCommand(serviceId);
+            var command = await _commandProvider.GetCommand(serviceId);
             var intervalSeconds = (DateTime.Now - serviceInvokeInfos.FinalRemoteInvokeTime).TotalSeconds;
             bool reachConcurrentRequest() => serviceInvokeInfos.ConcurrentRequests > command.MaxConcurrentRequests;
             bool reachRequestVolumeThreshold() => intervalSeconds <= 10
                 && serviceInvokeInfos.SinceFaultRemoteServiceRequests > command.BreakerRequestVolumeThreshold;
             bool reachErrorThresholdPercentage() =>
-                serviceInvokeInfos.FaultRemoteServiceRequests / (serviceInvokeInfos.RemoteServiceRequests??1)*100 > command.BreakeErrorThresholdPercentage;
+                serviceInvokeInfos.FaultRemoteServiceRequests / (serviceInvokeInfos.RemoteServiceRequests ?? 1) * 100 > command.BreakeErrorThresholdPercentage;
             if (command.BreakerForceClosed)
             {
                 _serviceInvokeListenInfo.AddOrUpdate(serviceId, new ServiceInvokeListenInfo(), (k, v) => { v.LocalServiceRequests++; return v; });
@@ -43,7 +43,7 @@ namespace Surging.Core.CPlatform.Support.Implementation
             {
                 if (reachConcurrentRequest() || reachRequestVolumeThreshold() || reachErrorThresholdPercentage())
                 {
-                    if (intervalSeconds*1000 > command.BreakeSleepWindowInMilliseconds)
+                    if (intervalSeconds * 1000 > command.BreakeSleepWindowInMilliseconds)
                     {
                         return await MonitorRemoteInvokeAsync(parameters, serviceId, serviceKey, decodeJOject, command.ExecutionTimeoutInMilliseconds);
                     }
@@ -55,7 +55,7 @@ namespace Surging.Core.CPlatform.Support.Implementation
                 }
                 else
                 {
-                    return await  MonitorRemoteInvokeAsync(parameters, serviceId, serviceKey, decodeJOject, command.ExecutionTimeoutInMilliseconds);
+                    return await MonitorRemoteInvokeAsync(parameters, serviceId, serviceKey, decodeJOject, command.ExecutionTimeoutInMilliseconds);
                 }
             }
             throw new NotImplementedException();
@@ -73,14 +73,14 @@ namespace Surging.Core.CPlatform.Support.Implementation
                     ++v.ConcurrentRequests;
                     return v;
                 });
-               var message = await _remoteInvokeService.InvokeAsync(new RemoteInvokeContext
+                var message = await _remoteInvokeService.InvokeAsync(new RemoteInvokeContext
                 {
                     InvokeMessage = new RemoteInvokeMessage
                     {
                         Parameters = parameters,
                         ServiceId = serviceId,
                         ServiceKey = serviceKey,
-                         DecodeJOject= decodeJOject,
+                        DecodeJOject = decodeJOject,
                     }
                 }, requestTimeout);
                 _serviceInvokeListenInfo.AddOrUpdate(serviceId, new ServiceInvokeListenInfo(), (k, v) =>
@@ -100,7 +100,7 @@ namespace Surging.Core.CPlatform.Support.Implementation
                     --v.ConcurrentRequests;
                     return v;
                 });
-                return   null;
+                return null;
             }
         }
     }
