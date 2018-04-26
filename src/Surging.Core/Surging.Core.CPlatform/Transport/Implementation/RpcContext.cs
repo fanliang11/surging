0d2f9f883a9789ff3c32087a9b8d86fbae4620e2
fanliang11 <137629448@qq.com>
2018-04-27 06:53:55 +0800

    add RpcContext

---
+++ src/Surging.Core/Surging.Core.CPlatform/Transport/Implementation/RpcContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Surging.Core.CPlatform.Transport.Implementation
+{
+    public class RpcContext
+    {
+        private ConcurrentDictionary<String, Object> contextParameters;
+
+        public ConcurrentDictionary<String, Object> GetContextParameters()
+        {
+            return contextParameters;
+        }
+
+        public void SetContextParameters(ConcurrentDictionary<String, Object> contextParameters)
+        {
+            this.contextParameters = contextParameters;
+        }
+
+        private static ThreadLocal<RpcContext> rpcContextThreadLocal = new ThreadLocal<RpcContext>(() =>
+        {
+            RpcContext context = new RpcContext();
+            context.SetContextParameters(new ConcurrentDictionary<string, object>());
+            return context;
+        });
+        
+        public static RpcContext GetContext()
+        {
+            return rpcContextThreadLocal.Value;
+        }
+
+        public static void RemoveContext()
+        {
+            rpcContextThreadLocal.Dispose();
+        }
+
+        private RpcContext()
+        {
+        }
+    }
+}
