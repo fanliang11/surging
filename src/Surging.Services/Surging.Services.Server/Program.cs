9d6344534996fb5a5e4393d27e1126ff6efe647d
yjy <sunriseyuen@qq.com>
2018-05-06 19:18:51 +0800

    --

--- src/Surging.Services/Surging.Services.Server/Program.cs
+++ src/Surging.Services/Surging.Services.Server/Program.cs
@@ -35,26 +35,26 @@ namespace Surging.Services.Server
                 {
                     builder.AddMicroService(option =>
                     {
-                        option.AddServiceRuntime();
-                        option.AddRelateService();
-                        option.AddConfigurationWatch();
+                        option.AddServiceRuntime()
+                        .AddRelateService()
+                        .AddConfigurationWatch()
                         //option.UseZooKeeperManager(new ConfigInfo("127.0.0.1:2181"));
-                        option.UseConsulManager(new ConfigInfo("127.0.0.1:8500"));
-                        option.UseDotNettyTransport();
-                        option.UseRabbitMQTransport();
-                        option.AddRabbitMQAdapt();
-                        option.AddCache();
-                        //option.UseKafkaMQTransport(kafkaOption =>
+                        .UseConsulManager(new ConfigInfo("127.0.0.1:8500"))
+                        .UseDotNettyTransport()
+                        .UseRabbitMQTransport()
+                        .AddRabbitMQAdapt()
+                        .AddCache()
+                        //.UseKafkaMQTransport(kafkaOption =>
                         //{
                         //    kafkaOption.Servers = "127.0.0.1";
                         //    kafkaOption.LogConnectionClose = false;
                         //    kafkaOption.MaxQueueBuffering = 10;
                         //    kafkaOption.MaxSocketBlocking = 10;
                         //    kafkaOption.EnableAutoCommit = false;
-                        //});
-                        //option.AddKafkaMQAdapt();
-                        //option.UseProtoBufferCodec(); 
-                        option.UseMessagePackCodec();
+                        //})
+                        //.AddKafkaMQAdapt()
+                        //.UseProtoBufferCodec()
+                        .UseMessagePackCodec();
                         builder.Register(p => new CPlatformContainer(ServiceLocator.Current));
                     });
                 })
