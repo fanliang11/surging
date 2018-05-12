13176e9049883cb35a036cb6e89fe073eeec578a
yjy <sunriseyuen@qq.com>
2018-05-12 08:44:10 +0800

    --

--- src/Surging.Services/Surging.Services.Server/Program.cs
+++ src/Surging.Services/Surging.Services.Server/Program.cs
@@ -1,25 +1,5 @@
-﻿using Autofac;
-using Microsoft.Extensions.Logging;
-using Surging.Core.Caching;
-using Surging.Core.Caching.Configurations;
-using Surging.Core.Codec.MessagePack;
-using Surging.Core.Consul;
-using Surging.Core.Consul.Configurations;
-using Surging.Core.CPlatform;
-using Surging.Core.CPlatform.Configurations;
-using Surging.Core.CPlatform.Utilities;
-using Surging.Core.DotNetty;
-using Surging.Core.EventBusKafka.Configurations;
-//using Surging.Core.EventBusKafka;
-using Surging.Core.EventBusRabbitMQ;
-using Surging.Core.Log4net;
-using Surging.Core.Nlog;
-using Surging.Core.ProxyGenerator;
-using Surging.Core.ServiceHosting;
-using Surging.Core.ServiceHosting.Internal.Implementation;
+﻿using Surging.Core.ServiceHosting.Internal.Implementation;
 using System;
-//using Surging.Core.Zookeeper;
-//using Surging.Core.Zookeeper.Configurations;
 using System.Text;
 
 namespace Surging.Services.Bootstrap
@@ -28,7 +8,6 @@ namespace Surging.Services.Bootstrap
     {
         static void Main(string[] args)
         {
-
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             var host = new ServiceHostBuilder()
                 .Bootstrap()
