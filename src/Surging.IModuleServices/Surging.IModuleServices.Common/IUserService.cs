bbd21e72fec6d98be0d732d82d5ea8dc0d891bcc
fanliang11 <137629448@qq.com>
2019-08-20 20:44:52 +0800

    optimization code

--- src/Surging.IModuleServices/Surging.IModuleServices.Common/IUserService.cs
+++ src/Surging.IModuleServices/Surging.IModuleServices.Common/IUserService.cs
@@ -64,7 +64,7 @@ namespace Surging.IModuleServices.Common
         /// <returns></returns>
         [Authorization(AuthType = AuthorizationType.JWT)]
         [Command(Strategy = StrategyType.Injection, ShuntStrategy = AddressSelectorMode.HashAlgorithm, ExecutionTimeoutInMilliseconds = 1500, BreakerRequestVolumeThreshold = 3, Injection = @"return 1;", RequestCacheEnabled = false)]
-        [InterceptMethod(CachingMethod.Get, Key = "GetUser", CacheSectionType = SectionType.ddlCache, L2Key= "GetUserId_{0}",  EnableL2Cache = true, Mode = CacheTargetType.Redis, Time = 480)]
+        [InterceptMethod(CachingMethod.Get, Key = "GetUserId_{0}", CacheSectionType = SectionType.ddlCache, L2Key= "GetUserId_{0}",  EnableL2Cache = true, Mode = CacheTargetType.Redis, Time = 480)]
         [ServiceRoute("{userName}")]
         Task<int> GetUserId(string userName);
 
