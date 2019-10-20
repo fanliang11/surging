3f71596f0d91b19f96339d01cdea454af85047eb
fanliang11 <137629448@qq.com>
2019-10-20 11:48:49 +0800

    Add support for grpc service host

---
+++ src/Surging.IModuleServices/Surging.IModuleServices.Common/IGreeterService.cs
@@ -0,0 +1,10 @@
+﻿using Surging.Core.CPlatform.Ioc;
+using Surging.Core.CPlatform.Runtime.Server.Implementation.ServiceDiscovery.Attributes;
+
+namespace Surging.IModuleServices.Common
+{
+    [ServiceBundle("api/{Service}/{Method}")]
+    public interface IGreeterService : IServiceKey
+    {
+    }
+}
---
+++ src/Surging.Modules/Surging.Modules.Common/Behaviors/GreeterBehavior.cs
@@ -0,0 +1,13 @@
+﻿using Greet;
+using Surging.Core.CPlatform.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Surging.Modules.Common.Behaviors
+{
+    public partial class GreeterBehavior : Greeter.GreeterBase, IServiceBehavior
+    {
+
+    }
+}
