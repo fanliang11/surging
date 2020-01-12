8236ad9fea8b660855397cc9e5a6c9d874fc76c1
bruce_hu <bruce_hu123@163.com>
2020-01-12 21:24:44 +0800

    添加serilog支持

---
+++ src/Surging.Core/Surging.Core.Serilog/SerilogModule.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Elasticsearch;
+using Surging.Core.CPlatform;
+using Surging.Core.CPlatform.Module;
+using Surging.Core.ServiceHosting.Internal;
+
+namespace Surging.Core.Serilog
+{
+    public class SerilogModule: EnginePartModule
+    {
+        public override void Initialize(AppModuleContext context)
+        {
+            var serviceProvider = context.ServiceProvoider;
+            base.Initialize(context);
+            var section = AppConfig.GetSection("Serilog");
+
+            var logger = new LoggerConfiguration().ReadFrom.Configuration(section)
+                //.WriteTo.RollingFile(new ElasticsearchJsonFormatter(renderMessageTemplate:false),"c:/logs/log-{Date}.log")
+                //.WriteTo.Logger(config => { 
+                //    config.Filter.ByIncludingOnly(evt=>evt.Level== Serilog.Events.LogEventLevel.Information).WriteTo.RollingFile()
+                //})
+                .CreateLogger();
+            
+            serviceProvider.GetInstances<ILoggerFactory>().AddSerilog(logger);
+            serviceProvider.GetInstances<IApplicationLifetime>().ApplicationStopped.Register(Log.CloseAndFlush);
+        }
+
+        //public override void Dispose()
+        //{
+        //    //Log.CloseAndFlush();
+        //    base.Dispose();
+        //}
+    }
+}
---
+++ src/Surging.Core/Surging.Core.Serilog/Serilogger.cs
@@ -0,0 +1,23 @@
+﻿//using Microsoft.Extensions.Logging;
+//using System;
+
+//namespace Surging.Core.Serilog
+//{
+//    public class Serilogger : Microsoft.Extensions.Logging.ILogger
+//    {
+//        public IDisposable BeginScope<TState>(TState state)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public bool IsEnabled(LogLevel logLevel)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
